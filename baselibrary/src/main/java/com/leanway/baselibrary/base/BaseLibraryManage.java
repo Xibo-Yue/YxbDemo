fe249224fe31fd3d6136a397776c683afbb753b5
yxb <yxb@leanway.com.cn>
2017-04-14 16:18:53 +0800

    aaa

--- baselibrary/src/main/java/com/leanway/baselibrary/base/BaseLibraryManage.java
+++ baselibrary/src/main/java/com/leanway/baselibrary/base/BaseLibraryManage.java
@@ -3,8 +3,6 @@ package com.leanway.baselibrary.base;
 import android.app.Application;
 import android.content.Context;
 
-import com.leanway.baselibrary.okhttp_hy.OKHttpManager;
-
 /**
  * @author Xibo_Yue
  * @time 2017/2/13 17:53
@@ -13,6 +11,5 @@ public class BaseLibraryManage{
     public static Context library_context;
     public static void baseLibraryInit(Application context){
         library_context = context;
-        OKHttpManager.initOkhttp(context);  //初始化Okhttp
     }
 }
---
+++ js_bridge/src/main/java/com/leanway/js_bridge/MyWebViewClient.java
@@ -0,0 +1,15 @@
+package com.leanway.js_bridge;
+
+import com.github.lzyzsd.jsbridge.BridgeWebView;
+import com.github.lzyzsd.jsbridge.BridgeWebViewClient;
+
+/**
+ * @author Xibo_Yue
+ * @time 2017/4/14 16:15
+ */
+public class MyWebViewClient extends BridgeWebViewClient {
+
+    public MyWebViewClient(BridgeWebView webView) {
+        super(webView);
+    }
+}
