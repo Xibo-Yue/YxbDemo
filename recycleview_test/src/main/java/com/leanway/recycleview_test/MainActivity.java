8fab2632d0a6f2dbd255949ea957b8f0183eee27
yxb <yxb@leanway.com.cn>
2017-04-18 10:49:18 +0800

    recycleview Test

--- recycleview_test/src/main/java/com/leanway/recycleview_test/MainActivity.java
+++ recycleview_test/src/main/java/com/leanway/recycleview_test/MainActivity.java
@@ -2,15 +2,17 @@ package com.leanway.recycleview_test;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    private RecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
 
     }
 }
