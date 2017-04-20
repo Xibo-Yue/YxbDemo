94fb35491124c09070e1ff532d0233105a95ec36
yxb <yxb@leanway.com.cn>
2017-04-20 17:52:23 +0800

    recycleview

--- recycleview_test/src/main/java/com/leanway/recycleview_test/MainActivity.java
+++ recycleview_test/src/main/java/com/leanway/recycleview_test/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
@@ -21,9 +22,29 @@ public class MainActivity extends AppCompatActivity {
         mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
         initData();
         mRecyclerView.addItemDecoration(new RecycleViewDivider(MainActivity.this,LinearLayoutManager.HORIZONTAL));
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
-        //mRecyclerView.setAdapter(new TestAdapter(MainActivity.this, mTestBeens));
+
         SimpleRecyclerAdapter recyclerAdapter = new SimpleRecyclerAdapter(mTestBeens,MainActivity.this);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+
+        mRecyclerView.setLayoutManager(layoutManager);
+        //mRecyclerView.setAdapter(new TestAdapter(MainActivity.this, mTestBeens));
+        mRecyclerView.setHasFixedSize(true);//当不需要动态计算item高度时，需要加上这句话
+
+
+        mRecyclerView.addOnScrollListener(new EndlessRecyclerOnScrollListener(layoutManager) {
+            @Override
+            public void onLoadMore(int currentPage) {
+                Log.i("page", currentPage+"");
+                simulateLoadMoreData();
+            }
+        });
+
+
+
+
+
+
         recyclerAdapter.setItemClickListener(new ItemClickListener() {
             @Override
             public void onItemClick(View view, int position) {
@@ -42,6 +63,14 @@ public class MainActivity extends AppCompatActivity {
         });
         mRecyclerView.setAdapter(recyclerAdapter);
 
+    }
+
+
+    /**
+     * 加载更多
+     */
+    private void simulateLoadMoreData() {
+        Toast.makeText(getApplicationContext(),"加载更多",Toast.LENGTH_SHORT).show();
     }
 
     private void initData() {
