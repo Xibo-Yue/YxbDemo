4f35fbb7fd4572a22cd3de55bb80c6b855459245
yxb <yxb@leanway.com.cn>
2017-06-01 12:29:17 +0800

    greendaoDemo

--- greendao/src/main/java/com/leanway/greendao/gen/DaoMaster.java
+++ greendao/src/main/java/com/leanway/greendao/gen/DaoMaster.java
@@ -22,15 +22,15 @@ public class DaoMaster extends AbstractDaoMaster {
     /** Creates underlying database table using DAOs. */
     public static void createAllTables(Database db, boolean ifNotExists) {
         CityDao.createTable(db, ifNotExists);
-        UserDao.createTable(db, ifNotExists);
         HeadDao.createTable(db, ifNotExists);
+        UserDao.createTable(db, ifNotExists);
     }
 
     /** Drops underlying database table using DAOs. */
     public static void dropAllTables(Database db, boolean ifExists) {
         CityDao.dropTable(db, ifExists);
-        UserDao.dropTable(db, ifExists);
         HeadDao.dropTable(db, ifExists);
+        UserDao.dropTable(db, ifExists);
     }
 
     /**
@@ -50,8 +50,8 @@ public class DaoMaster extends AbstractDaoMaster {
     public DaoMaster(Database db) {
         super(db, SCHEMA_VERSION);
         registerDaoClass(CityDao.class);
-        registerDaoClass(UserDao.class);
         registerDaoClass(HeadDao.class);
+        registerDaoClass(UserDao.class);
     }
 
     public DaoSession newSession() {
